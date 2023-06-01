dc305bf59c5f908b1e18ca0c3e36a6026e60cc10
o黯淡o <york8817612@hotmail.com>
2023-06-01 14:40:11 +0800

    add install prompt

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ window.onload = () => {
              .register('./sw.js');
   }
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+      // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        });
